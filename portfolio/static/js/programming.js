4c7d8b58d6d982af59f990e412d683bc55b1e312
nlovas <nlovas@ualberta.ca>
2017-09-13 16:19:51 -0600

    javascript now deals with images

--- portfolio/static/js/programming.js
+++ portfolio/static/js/programming.js
@@ -1,22 +1,42 @@
 
-function format(pData){
-	//makes point form work & inline html
+function format(pData,pImages){
+	//makes point form work, inline html, sets up images
 	var myPartsDiv;
 	var tmp;
+	var tmp2;
     for(i=0;i<pData.length;i++){
 
+        //for the bullets:
         myPartsDiv = document.createElement("div");
         myPartsDiv.className = "mypartsPP" + i;
+        myPartsDiv.innerHTML = "My roles included:<br/>" + pData[i]["myParts"];
+        //tmp = $(".languagesofPP" + i);
+        $(".languagesofPP" + i).after(myPartsDiv);
+        //for the description(so the links are formatted correctly, etc)
+        descDiv  = document.createElement("div");
+        descDiv.className = "descPP" + i;
+        descDiv.innerHTML = "Description:<br/>" + pData[i]["desc"];
+        //tmp2 = $(".sourcecodeofPP" + i);
+        $(".sourcecodeofPP" + i).after(descDiv);
 
+        //for the images
+        for(p=0;p<pImages[i].length;p++){
+            thumbnail = document.createElement("img");
+            thumbnail.className = "PP" + i + "image" + p;
+            $(thumbnail).attr("alt","Screenshot of " + pData[i]["title"]);
+            $(thumbnail).attr("src", "../static/images" + pImages[i][p]);
+            $(".PP" + i).append(thumbnail);
 
-        myPartsDiv.innerHTML = pData[i]["myParts"];
-        //console.log(myPartsDiv);
-        //tmp = ".languagesofPP" + i.toString() + "";
-        //console.log(tmp);
-        tmp = $(".languagesofPP" + i);
-        $(tmp).after(myPartsDiv);
+        }
+
     }
     //$("mypartsofPP");
 	//alert(pData[0]['title']);
 	// var pData = [{"myParts": "laaaaaaaaaaaalalalalalallalalalaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}];
+}
+
+function prepareImages(pImages){
+    //sets up the images on the page to behave properly
+
+
 }
